8732a9d48b353d12cc86f8b90613a98e14387114
datta sai <kuppadattasai@gmail.com>
2024-07-28 16:02:23 +0530

    Second Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is a Newsapp developed using ReactJS.This app uses an Api from [NewsApi](https://newsapi.org/).
+To run this Application follow the steps:
+npm intall
+npm run dev
--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -9,7 +9,7 @@ const NewsItem = (props) => {
           src={
             imageUrl
               ? imageUrl
-              : "https://www.hindustantimes.com/ht-img/img/2024/01/02/1600x900/Salaar_stills_6_2_1703240264249_1704212581297.jpg"
+              : "https://images.news18.com/ibnlive/uploads/2024/07/hp-snapdragon-x-laptops-india-2024-07-52962577639c4b10235e15c03e475d0f-16x9.jpg?impolicy=website&width=1200&height=675"
           }
           className="card-img-top"
           alt="..."
